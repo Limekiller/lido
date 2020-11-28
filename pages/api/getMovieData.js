ce278feeb9337ac2c98dfd6227efbf535fc2e984
Limekiller <me@bryceyoder.com>
2020-11-27 23:23:37 -0500

    Improve media detection

--- pages/api/getMovieData.js
+++ pages/api/getMovieData.js
@@ -1,7 +1,12 @@
 
 export default async (req, res) => {
 
-    const strippedTitle = req.query.title.split('.').slice(0, -1).join('.')
+    // Remove file extension
+    let strippedTitle = req.query.title.split('.').slice(0, -1).join('.')
+    
+    // Split at any four-digit numbers and get first part
+    strippedTitle = strippedTitle.split(/[0-9]{4}/)[0]
+    
     let data = await fetch('https://www.omdbapi.com/?apikey=' + process.env.OMDB_API_KEY + '&t=' + strippedTitle + '&plot=full')
 
     res.setHeader('Content-Type', 'application/json')
