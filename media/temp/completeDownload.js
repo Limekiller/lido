82701cd18b96218b74e37bdea028b4c77d1224dc
Limekiller <me@bryceyoder.com>
2020-12-20 01:39:23 -0500

    Fix issue where only one file is copied from temp folder

--- media/temp/completeDownload.js
+++ media/temp/completeDownload.js
@@ -1,9 +1,19 @@
 const fs = require('fs-extra')
 const path = require('path')
 const FileType = require('file-type');
+const { getVideoDurationInSeconds } = require('get-video-duration')
+
+// Get file length in seconds
+// This is to determine if we should keep the file or not
+// Some torrents include random other videos like trailers and stuff
+// Get that outta here!
+const getFileLength = async (filePath) => {
+  return await getVideoDurationInSeconds(filePath)
+}
+
 
 // Recursively search for files
-const getAllFiles = function(dirPath, arrayOfFiles) {
+const getAllFiles = (dirPath, arrayOfFiles) => {
   files = fs.readdirSync(dirPath)
 
   arrayOfFiles = arrayOfFiles || []
@@ -19,6 +29,10 @@ const getAllFiles = function(dirPath, arrayOfFiles) {
   return arrayOfFiles
 }
 
+
+// Usually, the last argument is the file name.
+// If the file name has spaces, it will result in multiple args
+// starting at index 4. Here we construct them back into one var
 const constructFilename = () => {
   let fileName = ''
   for (let i=4; i<process.argv.length; i++) {
@@ -27,34 +41,50 @@ const constructFilename = () => {
   return fileName
 }
 
-// Get media path and JSON from file location
-const fileName = constructFilename()
-const mediaPath = process.argv[1].split('/').slice(0, -2).join('/')
-const JSONString = fs.readFileSync(mediaPath + '/temp/downloads.json', 'utf8')
-let JSONObject = JSON.parse(JSONString)
 
-// Get temp folder and download ID from arg 4, passed by aria2
-// Then get the final location by referencing the ID in the JSON
-const downloadID = fileName.split('/media/temp')[1].split('/')[1]
-const downloadPath = fileName.split('/media/temp')[0] + '/media/temp/' + downloadID
-const finalPath = JSONObject[downloadID];
-
-// Search through temp folder for video files
+// Search through temp folder for video files over 5 min
 // and move them to the final location
-const allFiles = getAllFiles(downloadPath)
-allFiles.forEach((file, index) => {
-    (async () => {
-        const fileData = await FileType.fromFile(file);
-        if (fileData) {
-            if (fileData.mime.substr(0, 5) == 'video') {
-                fs.renameSync(file, mediaPath + finalPath + '/' + file.split('/').slice(-1)[0])
-                fs.removeSync(downloadPath)
-                delete JSONObject[downloadID]
+const filterFiles = async (downloadPath, mediaPath, finalPath) => {
+  const allFiles = getAllFiles(downloadPath)
 
-                const JSONToWrite = JSON.stringify(JSONObject)
-                fs.writeFileSync(mediaPath + '/temp/downloads.json', JSONToWrite)
-            }
-        }
-    })();
-})
+  await Promise.all(allFiles.map( async file => {
+    const fileData = await FileType.fromFile(file);
+    if (fileData) {
+      const isVideoFile = fileData.mime.substr(0, 5) == 'video'
+      const isMovie = await getFileLength(file) > 300 ? true : false
+
+      if (isVideoFile && isMovie) {
+          fs.renameSync(file, mediaPath + finalPath + '/' + file.split('/').slice(-1)[0])
+      }
+    }
+  }))
+}
+
+
+const main = async () => {
+  // Get media path and JSON from file location
+  const fileName = constructFilename()
+  const mediaPath = process.argv[1].split('/').slice(0, -2).join('/')
+  const JSONString = fs.readFileSync(mediaPath + '/temp/downloads.json', 'utf8')
+  let JSONObject = JSON.parse(JSONString)
+
+  // Get temp folder and download ID from arg 4, passed by aria2
+  // Then get the final location by referencing the ID in the JSON
+  const downloadID = fileName.split('/media/temp')[1].split('/')[1]
+  const downloadPath = fileName.split('/media/temp')[0] + '/media/temp/' + downloadID
+  const finalPath = JSONObject[downloadID];
+
+  await filterFiles(downloadPath, mediaPath, finalPath)
+
+  fs.removeSync(downloadPath)
+  delete JSONObject[downloadID]
+
+  const JSONToWrite = JSON.stringify(JSONObject)
+  fs.writeFileSync(mediaPath + '/temp/downloads.json', JSONToWrite)
+}
+
+
+main()
+
+
 
--- media/temp/onDownloadComplete.sh
+++ media/temp/onDownloadComplete.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
+# Get the directory this script is running in, 
+# no matter where its called from
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# CD into the directory and use the npm script to run the node file
+# so we have the all the right libraries installed
+# no matter where we're running from
 cd $DIR
 npm run completeDownload $1 $2 $3
