fb9c0ee5505f95d3829b86e1e9159e176243cc01
Bryce Yoder <me@bryceyoder.com>
2024-02-28 22:18:36 -0500

    Update torrents.js

--- pages/api/search/torrents.js
+++ pages/api/search/torrents.js
@@ -15,13 +15,15 @@ export default async (req, res) => {
 
     let results
     for (let source of sources) {
-        results = await source(searchQuery)
+        try {
+            results = await source(searchQuery)
 
-        // TODO: Consider edge case where if results are returned, but none of them have a positive seeder - leecher ratio,
-        // no results are displayed and other torrent sources are not used
-        if (results && results.length > 0) {
-            break
-        }
+            // TODO: Consider edge case where if results are returned, but none of them have a positive seeder - leecher ratio,
+            // no results are displayed and other torrent sources are not used
+            if (results && results.length > 0) {
+                break
+            }
+        } catch (error) {}
     }
 
     res.setHeader('Content-Type', 'application/json')
@@ -41,10 +43,10 @@ const scrapeGloTorrent = query => {
                 return
             }
 
-            const title = row.querySelector('td:nth-child(2)').textContent.trim() 
+            const title = row.querySelector('td:nth-child(2)').textContent.trim()
             const link = row.querySelector('td:nth-child(4) a').href
-            const seeders = row.querySelector('td:nth-child(6)').textContent 
-            const leechers = row.querySelector('td:nth-child(7)').textContent 
+            const seeders = row.querySelector('td:nth-child(6)').textContent
+            const leechers = row.querySelector('td:nth-child(7)').textContent
 
             let type;
             const category = row.querySelector('td:nth-child(1) a').href.split('?cat=')[1]
@@ -93,7 +95,7 @@ const scrapeExtraTorrent = query => {
             const leechers = row.querySelector('td:nth-child(7)').textContent
 
             let type;
-            const category = row.querySelector('td').textContent.trim() 
+            const category = row.querySelector('td').textContent.trim()
             if (category.includes('Movies')) {
                 type = 'Movie'
             } else if (category.includes('TV')) {
@@ -133,7 +135,7 @@ const scrapeTorrentGalaxy = query => {
             const leechers = row.querySelector('font[color="#ff0000"]').textContent
 
             let type;
-            const category = row.querySelector('div').textContent 
+            const category = row.querySelector('div').textContent
             if (category.includes('Movie')) {
                 type = 'Movie'
             } else if (category.includes('TV')) {
@@ -157,11 +159,11 @@ const scrapeTorrentGalaxy = query => {
 const scrapePirateBay = (query) => {
     //query = query.toLowerCase().split(" ").join("+").replace(/["']/g, '')
     query = query.toLowerCase().replace(/["']/g, '')
-    return fetch(`https://thepiratebay10.org/search/${query}/1/7/0`)
+    return fetch(`https://thepiratebay10.xyz/search/${query}/1/7/0`)
     .then(response => {return response.text()})
     .then(html => {
         let dom = new jsdom.JSDOM(html)
-        
+
         let results = [];
         dom.window.document.querySelectorAll('tbody tr').forEach((row, index) => {
             if (index == dom.window.document.querySelectorAll('tbody tr').length - 1) {
