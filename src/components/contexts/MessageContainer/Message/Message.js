12b166f20c831150f2f17d854a6289b416da99e9
Limekiller <me@bryceyoder.com>
2025-12-18 22:44:02 -0500

    Add indicators to modal dialogues

--- src/components/contexts/MessageContainer/Message/Message.js
+++ src/components/contexts/MessageContainer/Message/Message.js
@@ -1,26 +1,39 @@
 "use client"
 
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import MessageContext from '@/lib/contexts/MessageContext'
 
 import styles from './Message.module.scss'
+import Spinner from '@/components/ui/Spinner/Spinner'
 
 const Message = ({ data }) => {
     const messageFunctions = useContext(MessageContext)
+    const [processing, setProcessing] = useState(false)
 
     useEffect(() => {
         document.querySelector(`#submit-${data.id}`)?.focus()
     }, [])
 
-    return data.hideBoilerplate ? 
+    return data.hideBoilerplate ?
         <div id={`message-${data.id}`}>{data.body}</div>
-    :
-        <div className={`${styles.Message} messageParent`} id={`message-${data.id}`}>
+        :
+        <div className={`${styles.Message} messageParent ${processing ? styles.processing : ''}`} id={`message-${data.id}`}>
+
+            {processing ? <div className={styles.spinnerContainer}>
+                <Spinner />
+            </div> : ""}
+
             <h1>{data.title}</h1>
-            <div style={{marginTop: '1rem'}}>{data.body}</div>
+            <div style={{ marginTop: '1rem' }}>{data.body}</div>
             {!data.hideButtons ?
                 <div className={styles.actions}>
-                    <button id={`submit-${data.id}`} className="messageSubmit" onClick={data.onSubmit}>Ok</button>
+                    <button
+                        id={`submit-${data.id}`}
+                        className="messageSubmit"
+                        onClick={async () => {setProcessing(true); await data.onSubmit(); setProcessing(false)}}
+                    >
+                        Ok
+                    </button>
                     <button className="secondary" onClick={messageFunctions.popMessage}>Cancel</button>
                 </div>
                 : ""
