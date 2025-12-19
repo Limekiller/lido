ed6db61c4095f8ca09a0e100de74a93f5144635c
Limekiller <me@bryceyoder.com>
2025-12-18 23:15:22 -0500

    Remove records that share download from watch log

--- src/lib/actions/user/file.js
+++ src/lib/actions/user/file.js
@@ -4,12 +4,26 @@ import { prisma } from "@/lib/prisma"
 import { revalidatePath, refresh } from "next/cache"
 
 export const removeFromWatchLog = async (id, fileId) => {
+    // Remove all items from watch log that share a download id with this file
+    const file = await prisma.file.findFirst({
+        where: {
+            id: fileId
+        }
+    })
+    const downloadId = file.downloadId
+    const sisterFiles = await prisma.file.findMany({
+        where: {
+            downloadId: downloadId,
+            area: 'video'
+        }
+    })
+    const fileIds = sisterFiles.map(file => file.id)
     await prisma.WatchLog.deleteMany({
         where: {
-            AND: [
-                { fileId: fileId },
-                { userId: id == -1 ? null : id }
-            ]
+            fileId: {
+                in: fileIds
+            },
+            userId: id == -1 ? null : id
         }
     })
 
